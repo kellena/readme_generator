5dbb633a00a55cc2d33a3d41db398ad89254f20a
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-04-25 19:29:15 -0500

    added renderLicenseBadge function, will use same format for link and section

---
+++ README.md
@@ -0,0 +1,38 @@
+# README Generator
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
+
+  ## Table of Contents:
+
+  [Description](#description)
+  [Installation](#installation)
+  [Usage](#usage)
+  [Contributing](#contributing)
+  [Tests](#tests)
+  [License](#license)
+  [Questions](#questions)
+  [Contact](#contact)
+
+  # Description
+  test
+
+  # Installation
+  test
+
+  # Usage
+  test
+
+  # Contributing
+  test
+
+  # Tests
+  test
+
+  # License
+
+  # Questions
+
+  ### Contact
+  GitHub Profile: (https://github.com/kellena)
+  Email:   test@gmail.com
+  
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,12 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === 'MIT License') {
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (license === "Apache License") {
+    return `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
